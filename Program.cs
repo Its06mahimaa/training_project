ce5a470b6f1c23db4ad1564991c2353bbaa9b2df
Mahima Chauhan <92840571+Its06mahimaa@users.noreply.github.com>
2022-10-28 13:15:40 +0530

    api

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 global using trainingproject.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -13,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options => options.AddPolicy("myPolicy", policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Environment.ContentRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
